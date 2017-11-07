7ab678fad8d356ea8a90d329a8ab5cafccae23ff
paps272003 <paps272003@gmail.com>
2017-11-08 00:33:57 +0530

    Write a program to give the output in specified format

---
+++ Assignment2.4.py
@@ -0,0 +1,2 @@
+c="""WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constitute India into a SOVEREIGN,!\n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\n\t\tand to secure to all its citizens:"""
+print(c)
